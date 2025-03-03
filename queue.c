a1c4e59d85c77ab7cf3e312b3e724629465e7134
Simon Liu <simonliu423@gmail.com>
2025-03-03 08:50:58 +0000

    Implement delete middle node in queue
    Add implementation for q_delete_mid(). The function uses fast & slow
    pointer technique to find middle node, remove it from the list and
    release its memory.
    
    Change-Id: I43585bf1f418c6302556e3611b2d14bd974de2cb

--- queue.c
+++ queue.c
@@ -147,6 +147,22 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    /* Use fast & slow pointer to find the middle node */
+    struct list_head *tortoise, *hare;
+    tortoise = hare = head;
+
+    do {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+    } while (hare != head && hare->next != head);
+
+    /* Delete the element */
+    list_del(tortoise);
+    q_release_element(list_entry(tortoise, element_t, list));
+
     return true;
 }
 
